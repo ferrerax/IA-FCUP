dd02b482ad8879a868255f8ed8fc3087d9644ab5
El vostre nom <usuari@domini.com>
2022-04-22 00:01:43 +0100

    amb readme

--- trabalho2/config.hh
+++ trabalho2/config.hh
@@ -9,15 +9,15 @@
 #define CONFIG_HH_
 
 
-#define N_ROW 		6
-#define N_COLUMN	7
-#define N_NUMEROS N_ROW*N_COLUMN
+#define N_ROW 		6 				//Rows in table
+#define N_COLUMN	7				//Columns in table
+#define N_NUMEROS N_ROW*N_COLUMN	//Size of the tabulairo
 
 #define MINIMAX_MAX_UTILITY 100000000
-#define MINIMAX_DEPTH 2
+#define MINIMAX_DEPTH 6				//Max depth for minimax
 
-#define ALFABETA_DEPTH 7
+#define ALFABETA_DEPTH 6			//Max depth for alpha beta
 
-#define MCTS_ITERATIONS 10000
+#define MCTS_ITERATIONS 10000		//Iterations for MCTS
 
 #endif /* CONFIG_HH_ */
