04f28af9b41aedd87f4785b839fc1f990c818b5f
Marc Clascà <hulehule20@gmail.com>
2022-05-25 16:13:50 +0100

    Implementa constructores, set/get, addicio de fills, i printat de arbres

--- trabalho3/DecisionTree.cpp
+++ trabalho3/DecisionTree.cpp
@@ -1,11 +1,64 @@
 #include "DecisionTree.hh"
-
+#include<string>
 using namespace std;
 
-DecisionTree::DecisionTree(/* args */)
+DecisionTree::DecisionTree(string branch, DecisionTree *parent)
 {
+    
+}
+
+DecisionTree::DecisionTree(attribute_t at, string classname, node_t type) {
+    this->type = type;
+    if(type == ATTRIBUTE_NODE) {
+        this->attribute = at;
+        this->classValue = "";
+    } else {
+        this->classValue = classname;
+    }
+}
+
+DecisionTree::DecisionTree(istream &is)
+{
+    
 }
 
 DecisionTree::~DecisionTree()
 {
+}
+
+void DecisionTree::print_representation(ostream &os, string tab)
+{
+    if(type == ATTRIBUTE_NODE) {
+        if(parent != nullptr) os << endl;
+        os << tab << "<" << this->attribute.name << ">" << endl;
+    } else {
+        os << " " << this->classValue << " (" << countClassification << ")" << endl;
+    }
+
+    for (size_t i = 0; i < children.size(); i++)
+    {
+        os << tab << "    " << children[i]->branchValue << ":";
+        children[i]->print_representation(os, tab+"        ");
+    }
+    
+
+}
+
+void DecisionTree::addChild(DecisionTree * c, string branch) {
+    children.push_back(c);
+    c->setParent(this);
+    c->setBranch(branch);
+}
+
+void DecisionTree::setParent(DecisionTree *p) {
+    this->parent = p;
+}
+
+void DecisionTree::setBranch(string branchName) {
+    this->branchValue = branchName;
+}
+
+void DecisionTree::setCount(int count)
+{
+    countClassification = count;
 }
--- trabalho3/DecisionTree.hh
+++ trabalho3/DecisionTree.hh
@@ -1,9 +1,18 @@
-
 #ifndef DESTREE_H_
 #define DESTREE_H_
 
 #include <vector>
+#include <string>
 #include "Attribute.hh"
+#include <ostream>
+#include <istream>
+
+using namespace std;
+
+typedef enum {
+    ATTRIBUTE_NODE,
+    LEAF_NODE
+} node_t;
 
 class DecisionTree
 {
@@ -14,14 +23,23 @@ private:
 
     attribute_t attribute;
     string classValue;
+    int countClassification;
     string branchValue;
+    node_t type; // 0 -> attribute, 1 -> class
 
 public:
-    DecisionTree(/* args */);
+    DecisionTree(string branch, DecisionTree *parent);
+    DecisionTree(attribute_t at, string classname, node_t type);
+    DecisionTree(istream &is);
     ~DecisionTree();
 
-    void print_representation();
-    void store_tree();
+    void print_representation(ostream &os, string tab = "");
+
+    void addChild(DecisionTree * c, string branch);
+
+    void setParent(DecisionTree * p);
+    void setBranch(string branchName);
+    void setCount(int count);
 };
 
 #endif
